bd20c318b345c63c77f77987dbef73b62a49f2c4
sharib45 <sharibshahid45@gmail.com>
2024-05-05 17:10:12 +0530

    ss

---
+++ 1 star pattern.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+
+int main(){
+
+printf("Right Half Piramid is");
+
+printf("*\n");
+printf("**\n");
+printf("***\n");
+printf("****\n");
+printf("******S\n");
+
+
+
+
+
+
+
+}

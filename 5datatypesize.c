568961a5c9825aacff5a685062cf7d9f615f9540
sharib45 <sharibshahid45@gmail.com>
2024-05-20 06:49:17 +0530

    ss

---
+++ 5datatypesize.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+
+
+int integer;
+
+float decimal;
+
+double doub;
+
+char character;
+
+printf("The size of integeris %d bytes\n", sizeof(integer));
+
+printf("The size of integeris %d bytes\n ", sizeof(decimal));
+
+printf("The size of integeris %d bytes\n", sizeof(doub));
+
+printf("The size of integeris %d bytes\n", sizeof(character));
+
+
+
+
+
+}

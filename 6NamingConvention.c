da66bac8bee09ea72e3e68382b1ae793ed842d85
sharib45 <sharibshahid45@gmail.com>
2024-05-20 06:58:04 +0530

    ss

---
+++ 6NamingConvention.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+
+char first_name [20] = "Ram";
+
+char second_name[20]= "india";
+
+int age = 44;
+
+printf("First name is %s, last name is %s, Age is %d", first_name,second_name,age);
+
+
+
+
+
+
+
+
+}

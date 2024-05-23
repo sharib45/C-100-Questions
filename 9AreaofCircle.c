40a2edf62bd8fd0147f30c5f487289ef4448eea2
sharib45 <sharibshahid45@gmail.com>
2024-05-23 21:34:10 +0530

    ss

---
+++ 9AreaofCircle.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+
+int main(){
+
+float radius;
+
+float PI = 3.14;
+
+
+printf("enter the adius oof circle :  ");
+
+
+scanf("%f", &radius);
+
+float areaofcircle = 2*radius*radius*PI;
+
+
+printf("area of circle is %f", areaofcircle);
+
+
+
+
+
+}

e0585aaa3da2dca76559c88f0e5d8b8469d81f8f
sharib45 <sharibshahid45@gmail.com>
2024-05-23 20:49:00 +0530

    sss

---
+++ 8circlefermanceCircle.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main(){
+
+const float PI = 3.14;
+
+int radius ;
+
+float cicul;
+
+printf("please enter radius of circle :");
+
+scanf("%d", &radius);
+
+cicul = 2*PI*radius;
+
+printf("cirferculermance of circle is %f5 ", cicul);
+
+
+
+
+
+}

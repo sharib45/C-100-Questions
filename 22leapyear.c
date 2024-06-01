19de9cf5dd73db6d9817092a8868b3f4f1404c99
sharib45 <sharibshahid45@gmail.com>
2024-06-02 02:08:13 +0530

    xxx

--- 22leapyear.c
+++ 22leapyear.c
@@ -11,15 +11,56 @@ scanf("%d",&year);
 
 
 
-if(year%400==0){
+if(year%400==0 || (year%4==0 && year%100!=0)){
 
+printf("your year is leap year");
+}
 
+else {
 
-  
+printf("your year is not leap year");
+
 }
 
 
 
+
+
+
+
+
+
+// if(year%400==0){
+
+// printf("your year is leap year");
+
+  
+// }
+
+// else if(year%100==0){
+
+// printf("your year is not leap year");
+
+
+
+
+// }
+
+// else if (year%4==0){
+
+// printf("your year is leap year");
+
+
+
+// }
+
+
+// else{
+
+
+// printf("your year is not laep year");
+
+// }
 
 
 

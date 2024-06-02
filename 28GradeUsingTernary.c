4cc857773d1ca517d619afebac11ce0c7b5a0c1f
sharib45 <sharibshahid45@gmail.com>
2024-06-02 18:09:58 +0530

    hhgfhg

--- 28GradeUsingTernary.c
+++ 28GradeUsingTernary.c
@@ -1,1 +1,19 @@
 #include<stdio.h>
+
+
+
+int main(){
+
+int marks;
+
+
+printf("please enter your mark");
+
+scanf("%d",&marks);
+
+ marks>80 ? printf("High") : (marks >=50 ? printf("Moderate"): printf("low"));
+
+
+
+
+}

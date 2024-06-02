35f56cc9dfe04b99deadd808e02eb569a2149b98
sharib45 <sharibshahid45@gmail.com>
2024-06-02 18:44:13 +0530

    dfdf

---
+++ 30calculatorSwitch.c
@@ -0,0 +1,62 @@
+#include<stdio.h>
+
+
+int main(){
+
+printf("please enter two number to perform action:  ");
+
+
+float a ,b;
+
+char opr;
+
+float res;
+
+scanf("%f %f",&a,&b);
+
+printf("please enter opration : + or - or / or *:  ");
+
+scanf(" %c",&opr);
+
+int invalid=0;
+
+switch(opr){
+
+case '+':
+
+res = a+b;
+
+break;
+
+case '-':
+
+res = a-b;
+
+break;
+
+case '*':
+
+res = a*b;
+
+break;
+
+case '/':
+
+res = a/b;
+
+break;
+
+
+default:
+
+invalid=1;
+printf("please enter right key\n");
+
+break;
+}
+
+if(invalid==0){
+printf("result is %f",res);
+}
+
+}

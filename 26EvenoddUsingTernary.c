abe7f7c11dd59bdaa19c86eefd0e774aea748e0d
sharib45 <sharibshahid45@gmail.com>
2024-06-02 15:25:58 +0530

    sss

---
+++ 26EvenoddUsingTernary.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+
+int main(){
+
+int num;
+
+printf("please enter a number");
+
+
+scanf("%d",&num);
+
+
+num%2==0 ? printf("number is even") : printf("number is odd");
+
+
+
+}
---
+++ 27Absoulevalve.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+int num;
+
+printf("please enter the number");
+
+scanf("%d",&num);
+
+  num = num >0 ? num : -num;
+
+
+printf("the aboule number is %d", num);
+
+
+
+
+
+
+}

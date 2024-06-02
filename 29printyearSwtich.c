bcfe7d6be8957e6e4bc3ec5cc34202ce7ed2e09c
sharib45 <sharibshahid45@gmail.com>
2024-06-02 18:20:31 +0530

    sss

---
+++ 29printyearSwtich.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+
+
+int main(){
+
+int month;
+
+printf("please enter number of month");
+
+
+scanf("%d",&month);
+
+switch (month){
+
+
+case 1:
+
+printf("your month is jan");
+
+break;
+
+case 2:
+
+printf("your month is feb");
+
+break;
+
+
+
+default:
+
+printf("you enter wrong month number");
+
+}
+
+
+
+
+
+
+}

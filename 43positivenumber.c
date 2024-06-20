74fdc6ed276f6e3d19e38bcea1f3490f637842f9
sharib45 <sharibshahid45@gmail.com>
2024-06-20 16:29:57 +0530

    dfgfgdfgfd

---
+++ 43positivenumber.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+
+
+int main(){
+
+
+
+
+int num;
+
+do{
+printf("please enter a number");
+
+scanf("%d",&num);
+
+}
+
+while(num<=0);
+
+printf("you have enter postive number");
+
+
+
+
+return 0;
+
+}

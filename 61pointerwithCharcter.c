06ee115b8be82cea96b4fa4572c142c6cd9623e9
sharib45 <sharibshahid45@gmail.com>
2024-08-10 15:27:25 +0530

    gtrt

---
+++ 61pointerwithCharcter.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+
+int main(){
+
+
+char ch;
+
+char *ptr = &ch;
+
+printf("welcome to char pointer\n");
+
+printf("please enter character  : ");
+
+scanf("%c", ptr);
+
+
+printf("the valuve of num is %c",*ptr);
+
+printf("enter the valuve of is %c", ch);
+
+
+
+
+
+
+
+
+}
